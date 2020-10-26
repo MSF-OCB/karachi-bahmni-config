9bc792b46c8c4ad44387dc0bc1129af878f7f834
Mohammad Ullah (Munna) <munnabcis@gmail.com>
2020-10-26 15:04:50 +0100

    correct db name

--- openmrs/apps/reports/sql/patient_missed.sql
+++ openmrs/apps/reports/sql/patient_missed.sql
@@ -10,25 +10,25 @@ SELECT
     pa.date_created AS Appointment_Date,
     Status
 FROM
-    karachi_prod.patient_appointment pa
+    openmrs.patient_appointment pa
         INNER JOIN
-    `karachi_prod`.`person` AS PR ON PR.PERSON_ID = pa.PATIENT_ID
+    `openmrs`.`person` AS PR ON PR.PERSON_ID = pa.PATIENT_ID
         AND PR.voided = 0
         INNER JOIN
-    `karachi_prod`.`person_name` AS NM ON NM.PERSON_ID = pa.PATIENT_ID
+    `openmrs`.`person_name` AS NM ON NM.PERSON_ID = pa.PATIENT_ID
         AND NM.voided = 0
         AND preferred = 1
         INNER JOIN
-    `karachi_prod`.`patient_identifier` AS ID ON ID.PATIENT_ID = pa.PATIENT_ID
+    `openmrs`.`patient_identifier` AS ID ON ID.PATIENT_ID = pa.PATIENT_ID
         AND ID.IDENTIFIER_TYPE = 3
         AND ID.voided = 0
         AND ID.preferred = 1
         LEFT JOIN
-    `karachi_prod`.`patient_identifier` AS ID2 ON ID2.PATIENT_ID = pa.PATIENT_ID
+    `openmrs`.`patient_identifier` AS ID2 ON ID2.PATIENT_ID = pa.PATIENT_ID
         AND ID2.IDENTIFIER_TYPE = 4
         AND ID2.voided = 0
         LEFT JOIN
-    karachi_prod.person_attribute PA2 ON PA2.PERSON_ID = pa.PATIENT_ID
+    openmrs.person_attribute PA2 ON PA2.PERSON_ID = pa.PATIENT_ID
         AND PA2.person_attribute_type_id = 16
         LEFT join 
         registration as reg on reg.PATIENT_ID = ID.IDENTIFIER
@@ -36,7 +36,7 @@ WHERE
     pa.date_created = (SELECT 
             MAX(date_created)
         FROM
-            karachi_prod.patient_appointment
+            openmrs.patient_appointment
         WHERE
             patient_id = pa.patient_id)
         AND status = 'Missed';
